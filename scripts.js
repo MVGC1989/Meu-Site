d0e378a405313af24060c8eb95d5d0340b1dc367
Marcos Carvalho <63380367+MVGC1989@users.noreply.github.com>
2021-03-22 17:11:02 -0300

    Inicial Commit

---
+++ scripts.js
@@ -0,0 +1,27 @@
+const modal_overlay = document.querySelector(".modal_overlay")
+const cards = document.querySelectorAll(".card")
+
+for(let card of cards){
+    card.addEventListener("click", function() {
+        let id = card.getAttribute("id")
+        modal_overlay.classList.add("active")
+        modal_overlay.querySelector("iframe").src =`${id}`
+    })  
+} 
+const close_modal= document.querySelector(".close_modal")
+
+close_modal.addEventListener("click", function(){
+    modal_overlay.classList.remove("active")
+    modal_overlay.querySelector("a").src = "" 
+})
+
+const current_page = window.location.pathname
+const menu_items = window.document.querySelectorAll(" #active ")
+
+for( let item of menu_items){
+    if(current_page.includes(item.getAttribute("href"))){
+        item.classList.add("active")
+    }
+}
+
+
